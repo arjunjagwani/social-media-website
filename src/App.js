b23e0e03671b83ac04a0ef77d9aec91584e18d14
arjunjagwani <arjunjagwani6@gmail.com>
2025-09-16 02:47:02 +0530

    Added Like/Unlike functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import {Homepage} from "./pages/Home-page/homepage";
 import {Login} from "./pages/login";
 import {Navbar} from "./components/navbar";
-import {CreatePost} from "./pages/create-post/createpost"
+import {CreatePost} from "./pages/create-post/createpost";
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from './config/firebase';
+import { Navigate } from 'react-router-dom';
 
 function App() {
+  const [user]=useAuthState(auth);
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={user ? <Homepage /> : <Navigate to="/login" />} />
+          <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
           <Route path='/createpost' element={<CreatePost/>}/>
         </Routes>
       </Router>
--- src/pages/Home-page/post.js
+++ src/pages/Home-page/post.js
@@ -1,6 +1,44 @@
+import {db} from "../../config/firebase";
+import {collection,query, where} from "firebase/firestore";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../../config/firebase";
+import {addDoc,getDocs,deleteDoc,doc} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export const Post=(props)=>{
+    const likesRef=collection(db,"likes");
+    const [user]=useAuthState(auth);
     const {post}=props;
+    const likesDoc=query(likesRef,where("postId","==",post.id));
+    const [likes,setLikes]=useState(null);
+
+    const hasUserLiked=likes?.find((like)=>like.userId===user?.uid);
+
+
+    const getLikes=async()=>{
+        const data=await getDocs(likesDoc);
+        setLikes(data.docs.map((doc)=>({userId:doc.data().userId})));
+    }
+    const addLike=async ()=>{
+        
+        await addDoc(likesRef,{
+            postId:post.id,
+            userId:user?.uid
+        });
+        setLikes((prev)=>[...prev,{userId:user?.uid}]);
+    }
+    const removeLike=async ()=>{
+        const likeToDeleteQuery=query(likesRef,where("postId","==",post.id),where("userId","==",user?.uid));
+        const likeToDeleteData=await getDocs(likeToDeleteQuery);
+        const likeId=likeToDeleteData.docs[0].id
+        const likeToDelete=doc(db,"likes",likeId);
+        await deleteDoc(likeToDelete);
+        setLikes((prev)=>prev.filter((like)=>like.userId!==user?.uid));
+
+    }
+    useEffect(()=>{
+        getLikes();
+    },[])
     
     return(<div className="Post">
         <div className="UserName">
@@ -11,6 +49,12 @@ export const Post=(props)=>{
             </div>
         <div className="Description">
             <p>{post.description}</p>
+            </div>
+            <div className="Actions">
+                <button onClick={hasUserLiked?removeLike:addLike}>
+                    {hasUserLiked?<>&#128078;</>:<>&#128077;</>}
+                    </button>
+                <span>{likes?.length}</span>
             </div>
        
     </div>);
